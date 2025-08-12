3160b4d54fb737d4aed2fb9f83e254c6aa0048c7
Fanilo Andrianasolo <contact@andfanilo.com>
2025-08-12 13:12:10 +0200

    Start Gradio, though unfinished

--- README.md
+++ README.md
@@ -12,5 +12,6 @@ uv run pydytuesday tt-download 2025-07-29
 
 # Run apps
 uv run streamlit run streamlit/streamlit_app.py
+uv run gradio gradio/gradio_app.py
 ```
 
---
+++ gradio/gradio_app.py
@@ -0,0 +1,76 @@
+from python_webapp_comparison import get_data
+from python_webapp_comparison import get_hours_watch
+from python_webapp_comparison import get_num_movies
+from python_webapp_comparison import get_num_shows
+from python_webapp_comparison import get_num_views
+from python_webapp_comparison import get_periods
+from python_webapp_comparison import plot_detail_per_titles
+from python_webapp_comparison import plot_top_show_hours
+
+import polars as pl
+import gradio as gr
+
+df = get_data()
+
+css = """
+.card {
+    border: 1px;
+}
+"""
+
+
+def update_dataframe_preview(period):
+    return df.filter(pl.col("report") == period)
+
+
+with gr.Blocks(css=css) as demo:
+    gr.Markdown("# Movie Analytics Dashboard")
+
+    with gr.Row():
+        name_input = gr.Textbox(
+            label="Enter your name",
+            container=False,
+        )
+        hello_output = gr.Markdown("Provide your name")
+
+        @name_input.change(
+            inputs=[name_input],
+            outputs=[hello_output],
+        )
+        def update_greeting(name):
+            if not name:
+                return "Provide your name"
+            return f"Hello {name}!"
+
+    with gr.Row():
+        periods = get_periods().to_list()
+        selected_period = gr.Dropdown(
+            periods,
+            label="Select period:",
+            interactive=True,
+        )
+
+    with gr.Row():
+        with gr.Column(elem_classes="card"):
+            gr.Markdown("### Movies")
+            gr.Markdown(f"{get_num_movies(selected_period.value)}")
+        with gr.Column():
+            gr.Markdown("### Shows")
+            gr.Markdown(f"{get_num_shows(selected_period.value)}")
+        with gr.Column():
+            gr.Markdown("### Views")
+            gr.Markdown(f"{get_num_views(selected_period.value)}")
+        with gr.Column():
+            gr.Markdown("### Hours Watched")
+            gr.Markdown(f"{get_hours_watch(selected_period.value)}")
+
+    with gr.Row():
+        preview_dataframe = gr.DataFrame(update_dataframe_preview(periods[0]))
+
+    selected_period.change(
+        fn=update_dataframe_preview,
+        inputs=selected_period,
+        outputs=preview_dataframe,
+    )
+
+demo.launch()
