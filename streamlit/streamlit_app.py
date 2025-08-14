2effd5f568507ebb01347ebbb569d348a9b470e0
Fanilo Andrianasolo <contact@andfanilo.com>
2025-08-14 09:15:25 +0200

    Use session state for Streamlit

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -26,45 +26,64 @@ with app:
 title_row.title("Movie Analytics Dashboard")
 
 with greeting_row:
-    name = st.text_input("Enter name", width=250)
-    if not name:
+    st.text_input("Enter name", width=250, key="name")
+    if not st.session_state.name:
         st.markdown("Enter name", width="content")
     else:
-        st.markdown(f"Hello {name}!", width="content")
+        st.markdown(f"Hello {st.session_state.name}!", width="content")
 
 with filters_row:
     periods = get_periods()
-    selected_period = st.selectbox("Select Period: ", periods, width="stretch")
-    selected_content_type = st.radio(
-        "Select Type", ("movie", "show"), format_func=str.capitalize, width=300
+    content_types=("movie", "show")
+    st.selectbox("Select Period: ", periods, 0, width="stretch", key="selected_period")
+    st.radio(
+        "Select Type",
+        content_types,
+        0,
+        format_func=str.capitalize,
+        width=300,
+        key="selected_content_type",
     )
 
 with card_row:
     st.metric(
-        f"{selected_content_type.capitalize()}s",
-        get_num_elements(selected_period, selected_content_type),
+        f"{st.session_state.selected_content_type.capitalize()}s",
+        get_num_elements(
+            st.session_state.selected_period, st.session_state.selected_content_type
+        ),
         border=True,
     )
     st.metric(
-        "Views", get_num_views(selected_period, selected_content_type), border=True
+        "Views",
+        get_num_views(
+            st.session_state.selected_period, st.session_state.selected_content_type
+        ),
+        border=True,
     )
     st.metric(
         "Hours Watched",
-        get_hours_watch(selected_period, selected_content_type),
+        get_hours_watch(
+            st.session_state.selected_period, st.session_state.selected_content_type
+        ),
         border=True,
     )
 
 with chart_row:
-    fig = plot_velocity_plotly(selected_period, selected_content_type)
-    selected_points = st.plotly_chart(
+    fig = plot_velocity_plotly(
+        st.session_state.selected_period, st.session_state.selected_content_type
+    )
+    st.plotly_chart(
         fig,
         on_select="rerun",
         selection_mode="lasso",
-        key="plotly_chart",
+        key="selected_points",
     )
-    selected_titles = [p["hovertext"] for p in selected_points["selection"]["points"]]
 
-data_preview = preview_data(selected_period, selected_content_type, selected_titles)
+data_preview = preview_data(
+    st.session_state.selected_period,
+    st.session_state.selected_content_type,
+    [p["hovertext"] for p in st.session_state.selected_points["selection"]["points"]],
+)
 
 if data_preview.is_empty():
     st.info(
